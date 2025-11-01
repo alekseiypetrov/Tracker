3cb545aa92fa3aa5c4400061bb4a24a82eaf069b
alekseiypetrov <aleshapetrov03@gmail.com>
2025-11-01 17:08:07 +0300

    feat(ui): добавлен сепаратор на таббаром и конфигурация его цвета в зависимости от темы (светлая/темная)

--- Tracker/Presentation/TabBar/TabBarController.swift
+++ Tracker/Presentation/TabBar/TabBarController.swift
@@ -13,17 +13,39 @@ final class TabBarController: UITabBarController {
             .withTintColor(.ypGray)
         static let statisticsBarItemImageFilled = UIImage(named: "statistics_bar_item")?
             .withTintColor(.ypBlue)
+        static let separatorColor = UIColor(named: "SeparatorColor")
     }
+    
+    // MARK: - Separator
+    
+    private let separator: UIView = {
+        let view = UIView()
+        view.backgroundColor = Constants.separatorColor
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
     
     // MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .ypWhite
+        setupTabBar()
         setupViewControllers()
     }
     
     // MARK: - Private methods
+    
+    private func setupTabBar() {
+        view.backgroundColor = .ypWhite
+        view.addSubview(separator)
+        
+        NSLayoutConstraint.activate([
+            separator.leadingAnchor.constraint(equalTo: tabBar.leadingAnchor),
+            separator.trailingAnchor.constraint(equalTo: tabBar.trailingAnchor),
+            separator.bottomAnchor.constraint(equalTo: tabBar.topAnchor),
+            separator.heightAnchor.constraint(equalToConstant: 0.5)
+        ])
+    }
     
     private func setupViewControllers() {
         let trackerVC = TrackersViewController()
