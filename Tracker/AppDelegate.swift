4f98266f81840c7f5111e897cb2981b2b433732b
alekseiypetrov <aleshapetrov03@gmail.com>
2025-11-05 21:23:03 +0300

    feat(core): в класс AppDelegate добавлена инициализация CoreData Stack и сохранение контекста

--- Tracker/AppDelegate.swift
+++ Tracker/AppDelegate.swift
@@ -1,4 +1,5 @@
 import UIKit
+import CoreData
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -11,5 +12,31 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+    
+    // MARK: - CoreData Stack
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "ModelsCoreData")
+        container.loadPersistentStores(completionHandler: { description, error in
+            if let error = error as? NSError {
+                fatalError("Error in creation of Persistent Container: \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+    
+    // MARK: - CoreData Saving support
+
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
     }
 }
